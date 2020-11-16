9c53893aa539fdbaf600b5d005d66a4718f77ae2
Vincent Ivos <vincentivos1@gmail.com>
2020-11-16 12:40:13 -0500

    commit tree df680206dae8cf9c325383e4eb9da5e06f0b9dac

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,23 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
-end
+def key_for_min_value(name_hash)
+    if name_hash.size == 0
+      nil
+    else
+      a = []
+      name_hash.each do |x, y|
+        a.push(y)
+      end
+      c = a[0]
+      a.each do |d|
+        c = d if d < c
+      end
+      name_hash.each do |k, v|
+        if c == v
+         return k
+        end
+      end
+    end
+  end
